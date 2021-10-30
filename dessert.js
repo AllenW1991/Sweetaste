55f8f253d352ec8536a545f5075bba54fe3087e4
AllenW1991 <venecia631201@yahoo.com.tw>
2021-10-30 16:25:06 +0800

    update

--- dessert.js
+++ dessert.js
@@ -14,23 +14,53 @@ $(function () {
 
       $("#product1").text(number);
       $("#totalPrice1").text(`NT$ ` + product1Price);
-
     };
+    let price1Number = parseInt($("#product1").text());
+    let price2Number = parseInt($("#product2").text());
+    let price3Number = parseInt($("#product3").text());
 
+    let price1 = parseInt($("#productPrice1").text());
+    let price2 = parseInt($("#productPrice2").text());
+    let price3 = parseInt($("#productPrice3").text());
+
+    let totalPrice = price1Number * price1 + price2Number * price2 + price3Number * price3;
+    let addship = totalPrice + 300;
+    $("#priceNote").text(`NT$ ` + totalPrice);
+    $("#totalPrice").text(`NT$ ` + addship);
+
+    if (addship - 300 == 0) {
+      $("#totalPrice").text(`NT$ ` + 0);
+    }
 
   })
+
   $("#reduceNumber2").on("click", function () {
     let number = parseInt($("#product2").text());
     let price = parseInt($("#productPrice2").text());
 
     if (number >= 0, number == 0, number--) {
       number += "";
-      let product2Price = price * number;
 
       $("#product2").text(number);
       $("#totalPrice2").text(`NT$ ` + price * number);
 
-    };
+    }; let price1Number = parseInt($("#product1").text());
+    let price2Number = parseInt($("#product2").text());
+    let price3Number = parseInt($("#product3").text());
+
+    let price1 = parseInt($("#productPrice1").text());
+    let price2 = parseInt($("#productPrice2").text());
+    let price3 = parseInt($("#productPrice3").text());
+
+    let totalPrice = price1Number * price1 + price2Number * price2 + price3Number * price3;
+    let addship = totalPrice + 300;
+
+    $("#priceNote").text(`NT$ ` + totalPrice);
+    $("#totalPrice").text(`NT$ ` + addship);
+
+    if (addship - 300 == 0) {
+      $("#totalPrice").text(`NT$ ` + 0);
+    }
   })
   $("#reduceNumber3").on("click", function () {
     let number = parseInt($("#product3").text());
@@ -42,6 +72,24 @@ $(function () {
       $("#totalPrice3").text(`NT$ ` + price * number);
 
     };
+    let price1Number = parseInt($("#product1").text());
+    let price2Number = parseInt($("#product2").text());
+    let price3Number = parseInt($("#product3").text());
+
+    let price1 = parseInt($("#productPrice1").text());
+    let price2 = parseInt($("#productPrice2").text());
+    let price3 = parseInt($("#productPrice3").text());
+
+    let totalPrice = price1Number * price1 + price2Number * price2 + price3Number * price3;
+    let addship = totalPrice + 300;
+
+
+    $("#priceNote").text(`NT$ ` + totalPrice);
+    $("#totalPrice").text(`NT$ ` + addship);
+
+    if (addship - 300 == 0) {
+      $("#totalPrice").text(`NT$ ` + 0);
+    }
   })
 
 
@@ -53,6 +101,21 @@ $(function () {
     number += "";
     $("#product1").text(number);
     $("#totalPrice1").text(`NT$ ` + price * number);
+    let price1Number = parseInt($("#product1").text());
+    let price2Number = parseInt($("#product2").text());
+    let price3Number = parseInt($("#product3").text());
+
+    let price1 = parseInt($("#productPrice1").text());
+    let price2 = parseInt($("#productPrice2").text());
+    let price3 = parseInt($("#productPrice3").text());
+
+    let totalPrice = price1Number * price1 + price2Number * price2 + price3Number * price3;
+    let addship = totalPrice + 300;
+
+
+    $("#priceNote").text(`NT$ ` + totalPrice);
+    $("#totalPrice").text(`NT$ ` + addship);
+
   })
   $("#addNumber2").on("click", function () {
     let number = parseInt($("#product2").text());
@@ -61,6 +124,21 @@ $(function () {
     number += "";
     $("#product2").text(number);
     $("#totalPrice2").text(`NT$ ` + price * number);
+    let price1Number = parseInt($("#product1").text());
+    let price2Number = parseInt($("#product2").text());
+    let price3Number = parseInt($("#product3").text());
+
+    let price1 = parseInt($("#productPrice1").text());
+    let price2 = parseInt($("#productPrice2").text());
+    let price3 = parseInt($("#productPrice3").text());
+
+    let totalPrice = price1Number * price1 + price2Number * price2 + price3Number * price3;
+    let addship = totalPrice + 300;
+
+
+    $("#priceNote").text(`NT$ ` + totalPrice);
+    $("#totalPrice").text(`NT$ ` + addship);
+
   })
   $("#addNumber3").on("click", function () {
     let number = parseInt($("#product3").text());
@@ -69,6 +147,41 @@ $(function () {
     number += "";
     $("#product3").text(number);
     $("#totalPrice3").text(`NT$ ` + price * number);
+    let price1Number = parseInt($("#product1").text());
+    let price2Number = parseInt($("#product2").text());
+    let price3Number = parseInt($("#product3").text());
+
+    let price1 = parseInt($("#productPrice1").text());
+    let price2 = parseInt($("#productPrice2").text());
+    let price3 = parseInt($("#productPrice3").text());
+
+    let totalPrice = price1Number * price1 + price2Number * price2 + price3Number * price3;
+    let addship = totalPrice + 300;
+
+
+    $("#priceNote").text(`NT$ ` + totalPrice);
+    $("#totalPrice").text(`NT$ ` + addship);
   })
 
+
+
+  $(".fa-trash").on("click", function () {
+    $(this).parent().addClass("not-active");
+
+    let price1Number = parseInt($("#product1").text()); //2
+    let price2Number = parseInt($("#product2").text()); //2
+    let price3Number = parseInt($("#product3").text()); //2
+
+    let price1 = parseInt($("#productPrice1").text()); //120
+    let price2 = parseInt($("#productPrice2").text()); //150
+    let price3 = parseInt($("#productPrice3").text()); //150
+
+    let totalPrice = price1Number * price1 + price2Number * price2 + price3Number * price3;
+    // let lastTotal = totalPrice - $(this).prev()
+    let addship = totalPrice + 300;
+
+    $("#priceNote").text(`NT$ ` + totalPrice);
+    $("#totalPrice").text(`NT$ ` + addship);
+
+  })
 })
